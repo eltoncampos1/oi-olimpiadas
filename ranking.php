9db608db2c5eec61ea95b5d94663e1f49677d1fa
eltoncampos1 <eltoncampos36@gmail.com>
2021-07-16 12:49:50 -0300

    feat: create rankin page

---
+++ ranking.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <main class="form-return ranking-bg">
+        <header>
+            <div class="text-box-header">
+                <h1>GENTE DE FIBRA, <br>
+                    VIBRA. <br>
+                    <span>E TORCE JUNTO!</span>
+                </h1>
+
+                <p>
+                    Enquanto nossos atletas vão disputar a maior competição do mundo, você está convocado pra participar do nosso bolão. Tudo o que precisa fazer é montar o seu ranking com os países e as respectivas medalhas. Depois é só torcer pra que seu palpite esteja certo.
+                </p>
+            </div>
+
+            <div class="athlete-name">
+                <h1>
+                    Medina, <br>
+                    <span>Atleta de fibra</span>
+                </h1>
+            </div>
+        </header>
+        <section class="ranking congrats-bg deadline">
+            <div class="athlete-name footer congrats">
+                <h1>
+                    Leticia, <br>
+                    <span>Atleta de fibra</span>
+                </h1>
+            </div>
+
+            <div class="ranking-box">
+                <div class="ranking-description">
+                    <h1>
+                        Ranking
+                    </h1>
+
+                    <p>Tem muita competição pela frente. Boa sorte pra você e para os brasileiros na disputa.</p>
+                </div>
+
+                <div class="cooperator-ranking">
+                    <ul>
+                        <li>
+                            <div class="pod">
+                                <span>1°</span>
+                            </div>
+                            Nome do colaborador
+                        </li>
+                        <li>
+                            <div class="pod">
+                                <span>2°</span>
+                            </div>
+                            Nome do colaborador
+                        </li>
+                        <li>
+                            <div class="pod">
+                                <span>3°</span>
+                            </div>
+                            Nome do colaborador
+                        </li>
+                        <li>
+                            <div>
+                                <span> 4°</span>
+                            </div>
+                            Nome do colaborador
+                        </li>
+                        <li>
+                            <div>
+                                <span>5°</span>
+                            </div>
+                            Nome do colaborador
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
